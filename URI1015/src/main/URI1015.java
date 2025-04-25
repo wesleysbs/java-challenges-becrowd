9ccd46378415f865e489e0828cf4effa95f0066b
wesleysbs <wesleysbs@hotmail.com>
2025-04-25 10:47:10 -0300

    Distância entre dois pontos

--- URI1015/src/main/URI1015.java
+++ URI1015/src/main/URI1015.java
@@ -1,9 +1,26 @@
 package main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class URI1015 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double x1, y1, x2, y2, distancia;
+		
+		x1 = sc.nextDouble();
+		y1 = sc.nextDouble();
+		x2 = sc.nextDouble();
+		y2 = sc.nextDouble();
+		
+		distancia = Math.sqrt(Math.pow((x2-x1), 2.0) + Math.pow((y2-y1), 2.0));
+		
+		System.out.printf("%.4f\n", distancia);
+		
+		sc.close();
 
 	}
 

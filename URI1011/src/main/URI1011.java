2c81e87752c83fb53224c6765508856ea0dd7db6
wesleysbs <wesleysbs@hotmail.com>
2025-04-25 18:19:37 -0300

    Volume de uma esfera

--- URI1011/src/main/URI1011.java
+++ URI1011/src/main/URI1011.java
@@ -1,10 +1,25 @@
 package main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class URI1011 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double raio, volume;
+		double pi = 3.14159;
+		
+		raio = sc.nextDouble();
+		
+		volume = 4.0 / 3.0 * pi * Math.pow(raio, 3.0); 
+		
+		System.out.printf("VOLUME = %.3f\n", volume);
+		
+		sc.close();
 	}
-
 }

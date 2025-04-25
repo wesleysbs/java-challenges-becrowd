5d68f56d3f11dfc6339d863477fc2dbbb3ebb087
wesleysbs <wesleysbs@hotmail.com>
2025-04-25 11:36:20 -0300

    Idade em Dias

---
+++ URI1020/src/main/URI1020.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.Scanner;
+
+public class URI1020 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N, anos, meses, dias, resto;
+		
+		N = sc.nextInt();
+		
+		anos = N / 365;
+		resto = N % 365;
+		meses = resto / 30;
+		dias = resto % 30;
+		
+		System.out.println(anos + " ano(s)");
+		System.out.println(meses + " mes(es)");
+		System.out.println(dias + " dia(s)");
+		
+		sc.close();
+
+	}
+
+}

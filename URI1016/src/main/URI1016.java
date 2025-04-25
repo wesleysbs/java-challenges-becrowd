5416d45db55040c1a2e029ec0b4de11dbf5377b8
wesleysbs <wesleysbs@hotmail.com>
2025-04-25 18:22:46 -0300

    Distância entre dois carros

--- URI1016/src/main/URI1016.java
+++ URI1016/src/main/URI1016.java
@@ -1,10 +1,24 @@
 package main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class URI1016 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int n, resultado;
+	    
+		n = sc.nextInt();
+		
+		resultado = 2 * n;
+		
+		System.out.println(resultado + " minutos");
+		
+		sc.close();
 	}
-
 }

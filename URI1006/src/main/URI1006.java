148c23295e8c5e5f4ad46afa8d3b7795a230d669
wesleysbs <wesleysbs@hotmail.com>
2025-04-25 18:16:05 -0300

    Média 2

--- URI1006/src/main/URI1006.java
+++ URI1006/src/main/URI1006.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class URI1006 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double x, y, z, med;
+		
+		x = sc.nextDouble();
+		y = sc.nextDouble();
+		z = sc.nextDouble();
+		
+		med = (x*2.0+y*3.0+z*5.0) / 10.0;
+		
+		System.out.printf("MEDIA = %.1f\n", med);		
+		
+		sc.close();
 	}
-
 }

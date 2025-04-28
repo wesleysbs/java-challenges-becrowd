09e45873cc35a4a23029ab0ad13281350b88c694
wesleysbs <wesleysbs@hotmail.com>
2025-04-28 17:42:52 -0300

    Crescente e Decrescente

--- URI1113/src/main/URI1113.java
+++ URI1113/src/main/URI1113.java
@@ -7,24 +7,22 @@ public class URI1113 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int x = 1;
-		int y = 0;
+		 int x = sc.nextInt();
+		 int y = sc.nextInt();
 		
 		while (x != y) {
-			 x = sc.nextInt();
-			 y = sc.nextInt();
-			 
 			 if (x > y) {
 				 System.out.println("Decrescente");
 			 }
 			 
 			 else {
 				 System.out.println("Crescente");
-			 }		
+			 }
+			 
+			 x = sc.nextInt();
+			 y = sc.nextInt();
 		}
 		
 		sc.close();
-
 	}
-
 }

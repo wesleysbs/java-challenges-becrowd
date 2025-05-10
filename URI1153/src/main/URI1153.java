193b4f7a3a4b5b3cffb009c28621476c87dcd404
wesleysbs <wesleysbs@hotmail.com>
2025-05-10 10:47:11 -0300

    Fatorial Simples

---
+++ URI1153/src/main/URI1153.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Scanner;
+
+public class URI1153 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		int fat = 1;
+		for (int i=1; i<=n; i++) {
+			fat = fat * i;
+		}
+		
+		System.out.println(fat);
+		
+		sc.close();
+
+	}
+
+}
